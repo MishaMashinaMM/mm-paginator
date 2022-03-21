b3e0500b6d58ce159fcf4a8e6b92f3904e491400
MishaMashinaMM <50454597+MishaMashinaMM@users.noreply.github.com>
2022-03-21 14:45:44 +0100

    Add files via upload

---
+++ src/lib/mmpaginator.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MMPaginatorComponent } from './mmpaginator.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [MMPaginatorComponent],
+  imports: [CommonModule],
+  exports: [MMPaginatorComponent]
+})
+export class MMPaginatorModule { }
